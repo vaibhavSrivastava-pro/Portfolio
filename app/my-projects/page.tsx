d71d060b96c0bf954bb266cf90688cf2f2a9c46f
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-01-25 04:32:47 +0530

    lets see

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -3,18 +3,12 @@
 import ProjectCard from '@/components/ProjectCard'
 import { Projects } from '@/constants'
 import React from 'react'
-import Image from 'next/image'
 
 const Page = () => {
   return (
-    <div className='w-screen h-screen flex items-center justify-center bg-center bg-cover overflow-auto relative'>
-      <Image
-        src="/mountains.jpg"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        alt="Background Image"
-      />
+    <div
+    style={{backgroundImage: "url(/mountains.jpg)"}}
+     className='w-screen h-screen flex items-center justify-center bg-center bg-cover overflow-auto'>
       <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
         {Projects.map((project, index) => (
           <ProjectCard
