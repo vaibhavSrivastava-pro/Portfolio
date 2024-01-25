f2915e576694a5e4a579d5e55eacf13466d3cee4
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-01-26 03:47:31 +0530

    ui fixing

--- app/page.tsx
+++ app/page.tsx
@@ -47,25 +47,25 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
+      <div className="absolute  flex bottom-10 z-[60] right-5 flex-col md:hidden gap-5">
         <Link
           href="/my-skills"
-          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-sm text-white max-w-[200px]"
         >
           Skills
         </Link>
 
         <Link
           href="/my-projects"
-          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-sm text-white max-w-[200px]"
         >
-          Experience & PoW
+          Experience
         </Link>
         <Link
           href="/contact-me"
-          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-sm text-white max-w-[200px]"
         >
-          Contact me
+          Contact
         </Link>
       </div>
 
