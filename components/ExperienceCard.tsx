52b6f517af5383ae415b559897007b22555a53e2
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-01-26 03:30:59 +0530

    typo fix

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -3,7 +3,7 @@ const ExperienceCard = ({ company, role, duration, technologies }:{company:any, 
   return (
     <>
     <div className='bg-black bg-opacity-50 shadow-lg rounded-lg p-4 m-4 h-[200px] relative'>
-    <p className="absolute pr-5 right-0 text-gray-500 pt-10 hidden sm:block">Made RESTful apis <br /> Worked on Integrated LLMs <br />Worked on large sclae projects</p>
+    <p className="absolute pr-5 right-0 text-gray-500 pt-10 hidden sm:block">Made RESTful apis<br />Created interactive UIs<br />Integrated LLMs<br />Worked on large scale projects</p>
         <div>
       <h2 className='text-xl text-white font-bold mb-5'>{company}</h2>
       <p className='text-gray-500 mb-2'>{role}</p>
